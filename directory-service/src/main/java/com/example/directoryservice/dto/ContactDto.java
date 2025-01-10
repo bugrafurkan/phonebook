bff28b47b3b9ad83ee33d7f76c944f54e4288f76
bugrafurkan <bugraf.karabulut@gmail.com>
2025-01-10 16:15:15 +0300

    Entitylere göre DTO tanımlamaları yapıldı

--- directory-service/src/main/java/com/example/directoryservice/dto/ContactDto.java
+++ directory-service/src/main/java/com/example/directoryservice/dto/ContactDto.java
@@ -1,4 +1,16 @@
 package com.example.directoryservice.dto;
 
+import com.example.directoryservice.entity.ContactType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ContactDto {
+    private Long id;
+    private ContactType type;
+    private String contactDetail;
+    private String personId;
+
 }
+
