3aa7b3878555688c0540c8f6f675052dec7ea46b
bugrafurkan <bugraf.karabulut@gmail.com>
2025-01-10 15:59:51 +0300

    Dosya yapısındaki eksik dosyalar giderildi.Güncellendi entity tanımlamaları yapıldı

--- directory-service/src/main/java/com/example/directoryservice/entity/Person.java
+++ directory-service/src/main/java/com/example/directoryservice/entity/Person.java
@@ -1,4 +1,21 @@
 package com.example.directoryservice.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "persons")
 public class Person {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    public String firstName;
+    public String lastName;
+    public String company;
+
+    @OneToMany(mappedBy = "person", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Contact> constacts = new ArrayList<>();
 }
