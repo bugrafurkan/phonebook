5f126677b8e3105f69879cdb4e72a3779c578cff
bugrafurkan <bugraf.karabulut@gmail.com>
2025-01-12 04:35:11 +0300

    swaggerConfig ayarlamaları

--- directory-service/src/main/java/com/example/directoryservice/config/SwaggerConfig.java
+++ directory-service/src/main/java/com/example/directoryservice/config/SwaggerConfig.java
@@ -1,4 +1,29 @@
 package com.example.directoryservice.config;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.models.GroupedOpenApi;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public SwaggerConfig swaggerConfig() {
+        return new SwaggerConfig();
+    }
+
+    @Bean
+    public GroupedOpenApi directoryServiceOpenApi(){
+        return GroupedOpenApi.builder().
+                group("directory-service").
+                addOpenApiCustomizer(openApi -> {
+                    openApi.setInfo(new Info()
+                    .title("Directory Service API")
+                            .description("This is a Directory Service API")
+                            .version("v1.0")
+                            .license(new License().name("Apache 2.0")));
+                }).
+                build();
+    }
 }
